4ebc0b2075add57a644d39fbcc5b91cebb06562c
jpdf00 <j.pdf00@gmail.com>
2021-03-01 16:08:05 -0300

    Refactor Helper methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,31 +1,31 @@
 module PostsHelper
-  def show_post_list
-    render inline: '
-    <% @posts.each do |post| %>
-      <li>
-        <% if user_signed_in? %>
-          <strong><%= post.user.name %></strong><br>
-          <small>@<%= post.user.username %></small><br>
-        <% else %>
-          <strong>Anonymous</strong><br>
-        <% end %>
-        <p><%= post.body %></p>
-      </li><br>
-    <% end %>'
+  def show_post_list(posts)
+    out = ''
+    posts.each do |post|
+      out += '<li>'
+      if user_signed_in?
+        out += "<strong>#{post.user.name}</strong><br><small>@#{post.user.username}</small><br>"
+      else
+        out += "<strong>Anonymous</strong><br>"
+      end
+      out += "<p>#{post.body}</p>"
+      out += '</li><br>'
+    end
+    out.html_safe
   end
 
-  def error_block
-    render inline: '
-    <% if @post.errors.any? %>
-      <div id="error_explanation">
-        <h2><%= pluralize(@post.errors.count, "error") %> prohibited this post from being saved:</h2>
+  def show_validation_errors(post)
+    out = ''
+    if post.errors.any?
+      out += "<div id=\"error_explanation\"><h2>#{pluralize(@post.errors.count, "error")} prohibited this post from being saved:</h2><ul>"
 
-        <ul>
-          <% @post.errors.each do |error| %>
-            <li><%= error.full_message %></li>
-          <% end %>
-        </ul>
-      </div>
-    <% end %>'
+      post.errors.each do |error|
+        out += "<li>#{error.full_message}</li>"
+      end
+
+      out += '</ul>'
+      out += '</div>'
+    end
+    out.html_safe
   end
 end
